23037e211ed54f3b52ac94dd4636865fc41851b2
PhanQuocHuyIUH <22714321.huy@student.iuh.edu.vn>
2024-10-02 18:30:45 +0700

    dao feat

---
+++ src/DAO/NhanVien_Dao.java
@@ -0,0 +1,19 @@
+package DAO;
+
+public class NhanVien_Dao {
+    public void insert() {
+        System.out.println("Insert NhanVien");
+    }
+
+    public void update() {
+        System.out.println("Update NhanVien");
+    }
+
+    public void delete() {
+        System.out.println("Delete NhanVien");
+    }
+
+    public void select() {
+        System.out.println("Select NhanVien");
+    }
+}
