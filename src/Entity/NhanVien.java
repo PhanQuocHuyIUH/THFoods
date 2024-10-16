5edaca17767a73fd7855abb6739781ea30aac766
xuantruong01 <truongdzcr7@gmail.com>
2024-10-17 01:32:13 +0700

    done đăng nhập

--- src/Entity/NhanVien.java
+++ src/Entity/NhanVien.java
@@ -1,5 +1,85 @@
 package Entity;
 
-public class NhanVien {
+import java.time.LocalDate;
+import java.util.Objects;
 
+public class NhanVien {
+    private String maNV;
+    private String tenNV;
+    private String sdt;
+    private String email;
+    private LocalDate ngaySinh;
+    private TaiKhoan tenDangNhap;
+
+    public NhanVien() {
+    }
+
+    public NhanVien(String maNV, String tenNV, String sdt, String email, LocalDate ngaySinh, TaiKhoan tenDangNhap) {
+        this.maNV = maNV;
+        this.tenNV = tenNV;
+        this.sdt = sdt;
+        this.email = email;
+        this.ngaySinh = ngaySinh;
+        this.tenDangNhap = tenDangNhap;
+    }
+
+    public String getMaNV() {
+        return maNV;
+    }
+
+    public void setMaNV(String maNV) {
+        this.maNV = maNV;
+    }
+
+    public String getTenNV() {
+        return tenNV;
+    }
+
+    public void setTenNV(String tenNV) {
+        this.tenNV = tenNV;
+    }
+
+    public String getSdt() {
+        return sdt;
+    }
+
+    public void setSdt(String sdt) {
+        this.sdt = sdt;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getNgaySinh() {
+        return ngaySinh;
+    }
+
+    public void setNgaySinh(LocalDate ngaySinh) {
+        this.ngaySinh = ngaySinh;
+    }
+
+    public TaiKhoan getTenDangNhap() {
+        return tenDangNhap;
+    }
+
+    public void setTenDangNhap(TaiKhoan tenDangNhap) {
+        this.tenDangNhap = tenDangNhap;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof NhanVien nhanVien)) return false;
+        return Objects.equals(maNV, nhanVien.maNV);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(maNV);
+    }
 }
--- src/Gui/DangNhap.java
+++ src/Gui/DangNhap.java
@@ -1,15 +1,37 @@
 package Gui;
 
+import DAO.Admin_Dao;
+import DAO.NhanVien_Dao;
+import DB.Database;
+import Entity.NguoiQuanTri;
+import Entity.NhanVien;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class DangNhap extends JFrame implements ActionListener {
     private JButton loginButton;
     private  JPasswordField passwordText;
     private JTextField userText;
+
+    Admin_Dao admin_dao = new Admin_Dao();
+    NhanVien_Dao nhanVien_dao = new NhanVien_Dao();
+
+    private HashMap<String, String> adminUsers = new HashMap<>();
+    private HashMap<String, String> nvUser = new HashMap<>();
     public DangNhap() {
+
+        try {
+            Database.getInstance().connect();
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
         // Tạo JPanel chính với BorderLayout
         JPanel panel = new JPanel();
         panel.setLayout(new BorderLayout());
@@ -93,8 +115,35 @@ public class DangNhap extends JFrame implements ActionListener {
         Object obj = e.getSource();
         // Khi nhấn nút đăng nhập thành công sẽ hiện frame mới và đóng frame cũ đi
         if (obj == loginButton) {
-            new TrangChu();
-            dispose();
+            try {
+
+
+                ArrayList<NguoiQuanTri> admin = admin_dao.getInFor();
+                ArrayList<NhanVien> nhanVien = nhanVien_dao.getInForNV();
+                String userName = userText.getText();
+                char[] password = passwordText.getPassword();
+                String convertPassword = new String(password);
+                for (NguoiQuanTri ad : admin) {
+                    adminUsers.put(ad.getTenDangNhap(),ad.getMatKhau());
+                }
+                for(NhanVien nv : nhanVien) {
+                    nvUser.put(nv.getMaNV(),nv.getTenDangNhap().getmatKhau());
+                }
+
+                if(adminUsers.containsKey(userName) && adminUsers.get(userName).equals(convertPassword)) {
+                    new QuanLyTaiKhoan();
+                    this.dispose();
+                }else if(nvUser.containsKey(userName) && nvUser.get(userName).equals(convertPassword)){
+                    new TrangChu();
+                    this.dispose();
+                }else {
+                    JOptionPane.showMessageDialog(this, "Invalid Username or Password", "Login Failed", JOptionPane.ERROR_MESSAGE);
+                }
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(this, "Invalid Username or Password", "Login Failed", JOptionPane.ERROR_MESSAGE);
+            }
+
+
         }
     }
 }
