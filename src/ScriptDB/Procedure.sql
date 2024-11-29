9f20d6e983327c6aa9cfc0d42177a5e43d00f20c
xuantruong01 <truongdzcr7@gmail.com>
2024-11-29 22:04:53 +0700

    Lam trang CapTaiKhoan

--- src/ScriptDB/Procedure.sql
+++ src/ScriptDB/Procedure.sql
@@ -1,6 +1,6 @@
 use THFoods
 
-CREATE PROCEDURE getInforTkN
+CREATE PROCEDURE getInforTkNV
 AS
 BEGIN
     SELECT nv.maNV, nv.tenNV, nv.sdt, nv.email, nv.ngaySinh, tk.tenDangNhap, matKhau
@@ -8,5 +8,23 @@ BEGIN
     JOIN TaiKhoan tk ON nv.tenDangNhap= tk.tenDangNhap;
 END;
 
-EXECUTE getInforTkN
+EXECUTE getInforTkNV
+
+CREATE PROCEDURE getInforTkQL
+AS
+BEGIN
+    SELECT ql.maQL, ql.tenQL, ql.sdt, ql.email, tk.tenDangNhap, matKhau
+    FROM NguoiQuanLy ql
+    JOIN TaiKhoan tk ON ql.tenDangNhap= tk.tenDangNhap;
+END;
+
+EXECUTE getInforTkQL
+
+
+
+Delete from TaiKhoan
+where tenDangNhap = 'QL02'
+
+
+
 
